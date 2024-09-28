7dc0002d7475c1bc371af9d8e3798294b43993a0
ho3einHamidi <hosseinmhh@yahoo.com>
2024-09-28 17:54:34 +0330

    feat: auth page

---
+++ src/components/templates/CheckOtpForm.jsx
@@ -0,0 +1,29 @@
+import { checkOtp } from "../../services/auth";
+
+function CheckOtpForm({ code, setCode, setStep, mobile }) {
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    if (code.length !== 5) return;
+    const { response, error } = await checkOtp(mobile, code);
+    if (response) console.log(response);
+    if (error) console.log(error.response.data.message);
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <p>تایید کد پیامک شده</p>
+      <span>کد پیامک شده به شماره {mobile}را وارد کنید.</span>
+      <label htmlFor="input">کد تایید را وارد کنید</label>
+      <input
+        type="text"
+        id="input"
+        placeholder="کد تایید"
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+      />
+      <button type="submit">ورود</button>
+      <button onClick={() => setStep(1)}>تغییر شماره موبایل</button>
+    </form>
+  );
+}
+
+export default CheckOtpForm;
